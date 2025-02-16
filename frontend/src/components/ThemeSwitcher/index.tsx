ebda89433b210abcfa859991a1d00715eebdbc4b
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-02-16 05:39:30 +0000

    ThemeSwitcher using context

--- frontend/src/components/ThemeSwitcher/index.tsx
+++ frontend/src/components/ThemeSwitcher/index.tsx
@@ -1,3 +1,13 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../../context/ThemeContext';
+import { ThemeContextType } from '../../@types/theme';
+
 export function ThemeSwitcher() {
-  return <h1>THEME SWITCHER COMPONENT</h1>;
+  const { toggleTheme } = useContext<ThemeContextType>(ThemeContext);
+
+  return (
+    <button onClick={toggleTheme}>
+      <h1>THEME SWITCHER COMPONENT</h1>
+    </button>
+  );
 }

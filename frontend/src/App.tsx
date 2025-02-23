0562c501bafa59b1f2366a81d511e3e4379355f2
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-02-23 06:11:47 +0000

    Header component created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
+import { Header } from './components/Header';
 import { Router } from './routes/Router';
 import { GlobalStyle } from './styles/global';
 
@@ -6,6 +7,7 @@ export function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Router />
       </BrowserRouter>
 

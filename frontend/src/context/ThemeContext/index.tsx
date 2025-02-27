cd0efb9d3b2e94a97b268e5f37bdf0d2de63936f
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-02-27 09:46:10 +0000

    Theme typing definition

--- frontend/src/context/ThemeContext/index.tsx
+++ frontend/src/context/ThemeContext/index.tsx
@@ -1,18 +1,10 @@
-import { ReactNode, createContext, useState } from 'react';
+import { createContext, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { ThemeContextType, ThemeContextProps } from '../../@types/theme';
 import { DefaultTheme } from '../../@types/styles';
 import { ThemesNames } from '../../enums/ThemeSwitcher';
 import { LightTheme } from '../../styles/themes/light';
 import { DarkTheme } from '../../styles/themes/dark';
-
-interface ThemeContextType {
-  theme: DefaultTheme;
-  toggleTheme: () => void;
-}
-
-interface ThemeContextProps {
-  children: ReactNode;
-}
 
 export const ThemeContext = createContext({} as ThemeContextType);
 

c46027f7a54a057d5d735a7c5c78d65fd0b0b06a
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-02-28 07:35:31 +0000

    Profile photo added to Profile component

--- frontend/src/components/Profile/index.tsx
+++ frontend/src/components/Profile/index.tsx
@@ -1,11 +1,13 @@
 import { Networks } from '../Networks';
 import { ProfileInfo } from '../ProfileInfo';
 import { DownloadCV } from '../DownloadCV';
+import ProfilePhoto from '../../assets/profile/profilePhoto.jpeg';
 
 export function Profile() {
   return (
     <>
       <h1>PROFILE COMPONENT</h1>
+      <img src={ProfilePhoto} alt="Profile photo" />
       <Networks />
 
       <ProfileInfo />

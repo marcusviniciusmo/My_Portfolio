921be71267ae7d460e467871ab456dfe65e880fb
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-03-02 05:50:03 +0000

    Defined Outlet content styling

---
+++ frontend/src/layout/DefaultLayout/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const LayoutContainer = styled.div``;
+
+export const View = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  div.outletContent {
+    width: 84rem;
+  }
+`;

866fc6621be4fdeaba2d85f54771d6a0e4ac5104
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-03-05 20:15:12 +0000

    TitlePage typing created

--- frontend/src/components/TitlePage/index.tsx
+++ frontend/src/components/TitlePage/index.tsx
@@ -1,8 +1,5 @@
+import { TitlePageProps } from '../../@types/titlePage';
 import { Title } from './styles';
-
-interface TitlePageProps {
-  title: string;
-}
 
 export function TitlePage({ title }: TitlePageProps) {
   return <Title>{title}</Title>;

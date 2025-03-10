da99f2d8da279037da92ad72eb58385bf0fa583a
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-03-10 05:17:40 +0000

    Contact page styling

--- frontend/src/pages/Contact/index.tsx
+++ frontend/src/pages/Contact/index.tsx
@@ -1,10 +1,22 @@
 import { TitlePage } from '../../components/TitlePage';
+import { ContactContainer, Form, Text, Inputs, Submit } from './styles';
 
 export function Contact() {
   return (
-    <>
-      <h1>CONTACT PAGE</h1>
+    <ContactContainer>
       <TitlePage title="Contact" />
-    </>
+
+      <Form>
+        <Text>Thanks for taking the time to reach out.</Text>
+        <Inputs>
+          <input name="name" type="text" placeholder="Name" required/>
+          <input name="email" type="email" placeholder="Email" required/>
+        </Inputs>
+
+        <textarea name="message" placeholder="Message" required />
+
+        <Submit type='submit'>Submit</Submit>
+      </Form>
+    </ContactContainer>
   );
 }

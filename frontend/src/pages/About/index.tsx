5540ad499f0e73b1e3399e4b57b44bf67b3b3cbf
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-05-27 06:28:42 +0100

    About data created

--- frontend/src/pages/About/index.tsx
+++ frontend/src/pages/About/index.tsx
@@ -1,67 +1,10 @@
 import { useEffect, useState } from 'react';
-import {
-  Translate,
-  Code,
-  MobileFriendly,
-  Storage,
-  ViewKanban,
-  ThumbUpAltOutlined,
-} from '@mui/icons-material';
 import { TitlePage } from '../../components/TitlePage';
+import { ExpertisesData } from '../../data/About';
 import { ExpertiseType } from '../../@types/about';
 import { getIndexMap, setBorderColor } from '../../utils/Functions';
 import { borderColors } from '../../styles/global';
 import * as Styles from './styles';
-
-export const ExpertisesList: ExpertiseType[] = [
-  {
-    id: '1',
-    title: 'Languages I speak',
-    text: `React, React Native, TypeScript, JavaScript, HTML, CSS, C#, SQL
-      Server, Git`,
-    color: '#D565FF',
-    icon: Translate,
-  },
-  {
-    id: '2',
-    title: 'Web Development',
-    text: `Creating responsive and dynamic websites using the latest
-      technologies.`,
-    color: '#FE75D8',
-    icon: Code,
-  },
-  {
-    id: '3',
-    title: 'App Development',
-    text: `Developing intuitive and efficient mobile applications for Android
-      and iOS.`,
-    color: '#259FFF',
-    icon: MobileFriendly,
-  },
-  {
-    id: '4',
-    title: 'Backend & Storage',
-    text: `Implementing robust servers and secure, scalable storage solutions.`,
-    color: '#E6B85B',
-    icon: Storage,
-  },
-  {
-    id: '5',
-    title: 'Agile Methodologies',
-    text: `Utilizing agile methodologies to ensure quick and high-quality
-      deliveries.`,
-    color: '#FF6080',
-    icon: ViewKanban,
-  },
-  {
-    id: '6',
-    title: 'Good Practices',
-    text: `Adopting good programming practices to ensure clean, efficient, and
-      sustainable code.`,
-    color: '#8774FF',
-    icon: ThumbUpAltOutlined,
-  },
-];
 
 export function About() {
   const [expertisesList, setExpertisesList] = useState<ExpertiseType[]>([]);
@@ -75,11 +18,11 @@ export function About() {
     in the most creative way and adding value to you.`;
   
   useEffect(() => {
-    setExpertisesList(ExpertisesList);
+    setExpertisesList(ExpertisesData);
   }, []);
 
   useEffect(() => {
-    const map = getIndexMap(ExpertisesList);
+    const map = getIndexMap(ExpertisesData);
 
     setIndexMap(map);
   }, []);

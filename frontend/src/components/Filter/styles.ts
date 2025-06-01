df71201d806d0b57c17819bafb458073dfb4d41b
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-01 06:02:17 +0100

    Filter component styling

---
+++ frontend/src/components/Filter/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const FilterContainer = styled.div`
+  border: ${({ theme }) => `1px solid ${theme.styles.tertiaryColor}`};
+  width: 28.8rem;
+  margin-top: 2rem;
+  padding: 1.2rem;
+  border-radius: 0.8rem;
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+`;
+
+export const InputSearch = styled.input`
+  background: transparent;
+  border: 0;
+  flex: 1;
+  outline: none;
+`;

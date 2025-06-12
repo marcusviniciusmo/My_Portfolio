83040f44821633dc480cf5e50b118a71744d69cf
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-12 20:34:48 +0100

    First route returning response from Express

--- server/src/server/server.mjs
+++ server/src/server/server.mjs
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', () => {
-  console.log('Hello World');
+app.get('/', (request, response) => {
+  return response.send('Hello World');
 });
 
 app.listen(3333);

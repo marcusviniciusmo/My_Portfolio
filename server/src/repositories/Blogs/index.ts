a1be94213e86a1b215c9edd029bb8c02dcc552e4
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-21 13:16:11 +0100

    Exception logic to GetBlogsByUser route created

--- server/src/repositories/Blogs/index.ts
+++ server/src/repositories/Blogs/index.ts
@@ -1,11 +1,17 @@
 import { prisma } from '../../config/Repository';
+import { ThrowRepositoryException } from '../../utils/Functions';
 
-export const GetBlogsByUserRepository = async (userId: string) => {
+export const GetBlogsByUserRepository = async (
+  route: string,
+  userId: string,
+) => {
   try {
     const blogsByUser = await prisma.blogs.findMany({
       where: { user_ID: userId },
     });
 
     return blogsByUser;
-  } catch (error) {}
+  } catch (error) {
+    ThrowRepositoryException(route, userId);
+  }
 };

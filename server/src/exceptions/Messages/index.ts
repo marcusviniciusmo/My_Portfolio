54c729c4b1de4c613391bc37df789887cb9f6e79
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-27 19:34:59 +0100

    Exception logic to GetBlogsByUser route created

--- server/src/exceptions/Messages/index.ts
+++ server/src/exceptions/Messages/index.ts
@@ -1,22 +1,30 @@
 export const Controller = {
+  GetBlogsByUser: ({ userId }: { userId: string }) =>
+    `Failed to process blog(s) request for user ID ${userId}.`,
   GetProfileById: ({ userId }: { userId: string }) =>
     `Failed to process request for user ID ${userId}.`,
   CreateProfile: () => `Failed to process request to create new profile.`,
 };
 
 export const Service = {
+  GetBlogsByUser: ({ userId }: { userId: string }) =>
+    `Unable to retrieve blog(s) for user ID ${userId}.`,
   GetProfileById: ({ userId }: { userId: string }) =>
     `Unable to retrieve profile for user ID ${userId}.`,
   CreateProfile: () => `Unable to create new profile.`,
 };
 
 export const Repository = {
+  GetBlogsByUser: ({ userId }: { userId: string }) =>
+    `Failed to fetch blog(s) for user ID ${userId}.`,
   GetProfileById: ({ userId }: { userId: string }) =>
     `Failed to fetch profile for user ID ${userId}.`,
   CreateProfile: () => `Failed to create new profile.`,
 };
 
 export const NotFound = {
+  GetBlogsByUser: ({ userId }: { userId: string }) =>
+    `Blog(s) from user ID ${userId} not found.`,
   GetProfileById: ({ userId }: { userId: string }) =>
     `Profile with user ID ${userId} not found.`,
 };

b64af5a4d1e020dd177b65435885361f83976450
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-27 05:17:37 +0100

    JWT authentication logic created

---
+++ server/src/middlewares/Jwt/index.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const secretKey = process.env.SECRET_KEY!;
+
+export const AuthenticateJwt = (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  const authHeader = request.headers.authorization;
+
+  try {
+    const token = authHeader!.split(' ')[1];
+
+    const decoded = jwt.verify(token, secretKey);
+    request.decoded = decoded;
+
+    next();
+  } catch (error) {}
+};

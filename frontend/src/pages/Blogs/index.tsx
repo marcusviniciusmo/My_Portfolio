7cdc613511a339c33657fd7df1586f08d6319b26
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2025-06-28 04:48:21 +0100

    Blogs page getting data from API

--- frontend/src/pages/Blogs/index.tsx
+++ frontend/src/pages/Blogs/index.tsx
@@ -1,23 +1,30 @@
 import { useEffect, useState } from 'react';
 import { TitlePage } from '../../components/TitlePage';
 import { Filter } from '../../components/Filter';
-import { blogsData } from '../../data/Blogs';
 import { BlogType } from '../../@types/blogs';
 import { getIndexMap, setBorderColor } from '../../utils/Functions';
 import { borderColors } from '../../styles/global';
 import { BlogsContainer, Content, Blog, Box, Image, Name } from './styles';
 
 export function Blogs() {
-  const [blogsList, setBlogsList] =  useState<BlogType[] | []>(blogsData);
-  const [blogsFiltered, setBlogsFiltered] =  useState<BlogType[]>(blogsList);
+  const [blogsList, setBlogsList] = useState<BlogType[]>([]);
+  const [blogsFiltered, setBlogsFiltered] = useState<BlogType[]>(blogsList);
   const [isListInHover, setIsListInHover] = useState<boolean>(false);
   const [isItemInHover, setIsItemInHover] = useState<string | null>(null);
-  const [indexMap, setIndexMap] = useState<Map<string, number>>(
-    new Map()
-  );
+  const [indexMap, setIndexMap] = useState<Map<string, number>>(new Map());
+
+  const baseUrlApi = import.meta.env.VITE_BASE_URL_API;
+  const userIdProfile = import.meta.env.VITE_USER_ID_PROFILE;
 
   useEffect(() => {
-    setBlogsList(blogsData);
+    fetch(`${baseUrlApi}/blogs/${userIdProfile}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setBlogsList(data);
+      })
+      .catch((error) => {
+        console.log(`Error: ${error}`);
+      });
   }, []);
 
   useEffect(() => {
@@ -31,11 +38,11 @@ export function Blogs() {
   }
 
   function handleMouseEnterList(isHover: boolean) {
-    setIsListInHover(isHover)
+    setIsListInHover(isHover);
   }
 
   function handleMouseEnterItem(itemId: string | null) {
-    setIsItemInHover(itemId)
+    setIsItemInHover(itemId);
   }
 
   return (
@@ -48,8 +55,9 @@ export function Blogs() {
         onMouseEnter={() => handleMouseEnterList(true)}
         onMouseLeave={() => handleMouseEnterList(false)}
       >
-        {
-          blogsFiltered.map((blog) => {
+        {blogsFiltered
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .map((blog) => {
             return (
               <Blog
                 key={blog.id}
@@ -61,16 +69,15 @@ export function Blogs() {
                 onMouseLeave={() => handleMouseEnterItem(null)}
               >
                 <Box>
-                  <Image src={blog.image} alt={blog.name} />
+                  <Image src={`${baseUrlApi}/${blog.image}`} alt={blog.name} />
                 </Box>
 
-                <a href={blog.url} target='_blank'>
+                <a href={blog.url} target="_blank">
                   <Name>{blog.name}</Name>
                 </a>
               </Blog>
-            )
-          })
-        }
+            );
+          })}
       </Content>
     </BlogsContainer>
   );
